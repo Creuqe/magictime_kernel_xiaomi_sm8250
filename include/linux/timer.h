b0fccdc43be226aaf1e82cfd65000519eb40795e
Creuqe <ninad19999@gmail.com>
2024-05-22 17:36:39 +0100

    Revert "Merge remote-tracking branch 'lineage/lineage-20' into magictime-new"
    This reverts commit 4982fc0aad0cc80fe12353d4dd434beaeb6c574e, reversing
    changes made to a437a03c6077a08ffbba7b2958cf65b40363c15d.

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -184,7 +184,11 @@ extern int try_to_del_timer_sync(struct timer_list *timer);
 
 extern struct timer_base timer_base_deferrable;
 
-extern int del_timer_sync(struct timer_list *timer);
+#ifdef CONFIG_SMP
+  extern int del_timer_sync(struct timer_list *timer);
+#else
+# define del_timer_sync(t)		del_timer(t)
+#endif
 
 #define del_singleshot_timer_sync(t) del_timer_sync(t)
 
